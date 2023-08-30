3afcc89a6f92b25891478be41e7634b521067b4c
Mario Vrevic <mv2020@tobii.com>
2023-08-30 18:38:31 +0200

    Zadatak 2

--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -3,8 +3,8 @@ import { useState } from "react";
 const AnimalList = () => {
   const [animals, setAnimals] = useState([
     { species: "mamel", name: "whale", birthday: new Date() },
-    { species: "mamel", name: "elephant", birthday: new Date() },
-    { species: "mamel", name: "lion", birthday: new Date() },
+    { species: "mamel", name: "elephant" },
+    { species: "mamel", name: "lion" },
     { species: "bird", name: "owl", birthday: new Date() },
     { species: "bird", name: "eagel", birthday: new Date() },
   ]);
@@ -22,7 +22,11 @@ const AnimalList = () => {
           <tr key={index}>
             <td>{animal.species}</td>
             <td>{animal.name}</td>
-            <td>{animal.birthday.toLocaleDateString()}</td>
+            <td>
+              {animal.birthday
+                ? animal.birthday.toLocaleDateString()
+                : "Nepoznat"}
+            </td>
           </tr>
         ))}
       </tbody>
